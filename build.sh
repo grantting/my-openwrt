345dfdfeb3c5be8ade86e2d5f8ca78953fc5f7e7
ding <dhq.s@qq.com>
2024-08-24 11:29:09 +0800

    fix1129

--- build.sh
+++ build.sh
@@ -3,8 +3,20 @@
 # 打印 info
 make info
 
-# 使用 $PROFILE 变量
-PROFILE="$PROFILE"
+# 检查是否至少有一个参数传递进来
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 --profile <profile_name>"
+    exit 1
+fi
+
+# 获取 --profile 参数之后的值
+profile=$1
+
+# 如果 --profile 后面跟着正确的参数，则将其赋值给 PROFILE 变量
+if [[ $profile == "--profile"* ]]; then
+    PROFILE=${profile#*=}
+fi
+
 
 # 主配置名称
 # PROFILE="qihoo_360t7"
