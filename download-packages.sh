f9e94929b28a3801eb328de6f350c735fb170254
ding <dhq.s@qq.com>
2024-08-25 15:45:23 +0800

    5

--- download-packages.sh
+++ download-packages.sh
@@ -13,12 +13,18 @@ mkdir -p "$TARGET_DIR"
 # Function to download a package
 download_package() {
   local package_name=$1
-  local file_name=$(wget -qO- "$BASE_URL" | grep -oP "${package_name}.*?\.ipk")
-  if [ -n "$file_name" ]; then
-    xargs wget "${BASE_URL}${file_name}" -P "${TARGET_DIR}"
-    echo "Downloaded ${file_name}"
+  local full_url="$BASE_URL$(wget -qO- "$BASE_URL" | grep -oP "${package_name}.*?\.ipk")"
+  
+  if [ -n "$full_url" ]; then
+    local file_name=$(basename "$full_url")
+    if [ ! -f "$TARGET_DIR/$file_name" ]; then
+      wget -q --show-progress "$full_url" -P "$TARGET_DIR"
+      echo "Downloaded $full_url"
+    else
+      echo "$file_name already exists, skipping download."
+    fi
   else
-    echo "File not found for ${package_name}"
+    echo "File not found for $package_name"
   fi
 }
 
