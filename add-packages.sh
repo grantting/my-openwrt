07b6b7ba5919061ff3f1ee31afafe5de6470aac6
ding <dhq.s@qq.com>
2024-08-25 14:53:21 +0800

    1

--- add-packages.sh
+++ add-packages.sh
@@ -10,10 +10,3 @@ mkdir -p "$TARGET_DIR"
 
 # Get the page content and find the IPK file name
 FILE_NAME=$(wget -qO- "$BASE_URL" | grep -oP 'luci-app-openclash_.*?_all\.ipk')
-
-# Download the file if found
-if [ -n "$FILE_NAME" ]; then
-  wget "${BASE_URL}${FILE_NAME}" -O "${TARGET_DIR}/luci-app-openclash.ipk"
-else
-  echo "File not found"
-fi
